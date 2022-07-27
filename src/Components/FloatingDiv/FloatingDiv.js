fe7f5400ba15921d1beb37d64be371e5cd45da5a
Shubham Sati <shubham.sati8b@gmail.com>
2022-07-27 22:03:08 +0530

    Intro Section

---
+++ src/Components/FloatingDiv/FloatingDiv.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import './FloatingDiv.css';
+
+function FloatingDiv( {image , text1 , text2} ) {
+  return (
+    <div className='floatingdiv'>
+        <img src={image} alt='' />
+        <span>
+            {text1}
+            <br />
+            {text2}
+        </span>
+        <img src='' alt='' />
+
+    </div>
+  )
+}
+
+export default FloatingDiv
+
+
+
